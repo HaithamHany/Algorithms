3312d94204831160a600367448085e0bd96cf02b
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-04-15 20:18:13 -0400

    Stack and Queue

---
+++ Algorithims/DataStructure/Queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithims.DataStructure
+{
+	public class Queue<T>
+	{
+		private QueueNode _first;
+		private QueueNode _last;
+
+		private  class QueueNode
+		{
+			private T _data;
+			private QueueNode _next;
+
+			public T Data { get { return _data; } set { _data = value; } }
+			public QueueNode Next { get { return _next; } set { _next = value; } } 
+
+			public QueueNode(T data)
+			{
+				this._data = data;
+			}
+		}
+
+		/// <summary>
+		/// Adding new item to the queue
+		/// Time Complexity is O(1)
+		/// </summary>
+		/// <param name="item">Item to be added</param>
+		public void Enqueue(T item)
+		{
+			QueueNode newNode = new QueueNode(item);
+			if(_last != null)
+			{
+				_last.Next = newNode;
+			}
+			_last = newNode;
+			if (_first == null)
+				_first = _last;
+		}
+
+		/// <summary>
+		/// Removing the last element and returning it
+		/// Time Complexity is O(1)
+		/// </summary>
+		/// <returns></returns>
+		public T Dequeue()
+		{
+			if (_first == null)
+				throw new Exception("Queue is empty");
+
+			T data = _first.Data;
+			_first = _first.Next;
+
+			if (_first == null)
+				_last = null;
+
+			return data;
+		}
+
+		/// <summary>
+		/// Returning First item without removing it
+		/// Time Complexity is O(1)
+		/// </summary>
+		/// <returns></returns>
+		public T peek()
+		{
+			if (_first == null)
+				throw new Exception("Queue is empty");
+			return _first.Data;
+		}
+
+		/// <summary>
+		/// Checking if Queue is Empty
+		/// </summary>
+		/// <returns></returns>
+		public bool IsEmpty()
+		{
+			return _first == null;
+		}
+	}
+}
---
+++ Algorithims/DataStructure/Stack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithims.DataStructure
+{
+	/// <summary>
+	/// Linked-List based Stack
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class Stack<T>
+	{
+
+		private StackNode _top;
+		private StackNode Top { get; set; }
+
+		private  class StackNode
+		{
+			private T _data;
+			private StackNode _next;
+
+			public StackNode(T data)
+			{
+				this._data = data;
+			}
+
+			public T Data { get { return _data; } }
+
+			public StackNode Next { get { return _next; } set { _next = value; } }
+		}
+
+		/// <summary>
+		/// Popping last Top element
+		/// Time Complixity is O(1)
+		/// </summary>
+		/// <returns></returns>
+		public T Pop()
+		{
+			if(_top == null )
+				throw new Exception("Stack Is Empty");
+
+			T item = _top.Data;
+			_top = _top.Next;
+
+			return item;
+		}
+
+		/// <summary>
+		/// Inserting Element in the top of the Stack
+		/// Time Complexity is O(1)
+		/// </summary>
+		/// <param name="item">Item to be added</param>
+		public void Push( T item)
+		{
+			StackNode newNode = new StackNode(item);
+			newNode.Next = _top;
+			_top = newNode;
+		}
+
+		/// <summary>
+		/// Returning the top element without removing it
+		/// Time Complexity is O(1)
+		/// </summary>
+		/// <returns></returns>
+		public T Peek()
+		{
+			if (_top == null)
+				throw new Exception(" Stack is EMpty");
+
+			return _top.Data;
+		}
+
+		/// <summary>
+		/// Checking if Stack is Empty
+		/// </summary>
+		/// <returns></returns>
+		public bool IsEmpty()
+		{
+			return _top == null;
+		}
+		
+	}
+}
