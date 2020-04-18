057c777f7574b1b9043f386078c2b16d78c020f7
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-04-18 17:48:33 -0400

    Binary search Tree

---
+++ Algorithims/DataStructure/BST.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithims.DataStructure
+{
+	/// <summary>
+	/// Implementation for Binary Search Tree
+	/// </summary>
+	public class BST
+	{
+		private BSTNode _root;
+
+		public BSTNode Root { get { return _root; } }
+
+		public BST()
+		{
+			_root = null;
+		}
+
+		public void Insert(int data)
+		{
+			BSTNode newNode = new BSTNode();
+			newNode.Data = data;
+
+			if (_root == null)
+				_root = newNode;
+			else
+			{
+				BSTNode currentNode = _root;
+				BSTNode parent;
+
+				while (true)
+				{
+					parent = currentNode;
+					if(data < currentNode.Data)
+					{
+						currentNode = currentNode.Left;
+						if(currentNode ==null)
+						{
+							parent.Left = newNode;
+							break;
+						}
+					}
+					else
+					{
+						currentNode = currentNode.Right;
+						if(currentNode == null)
+						{
+							parent.Right = newNode;
+							break;
+						}
+					}
+
+				}
+			}
+
+		}
+
+		public void TraverseInOrder(BSTNode root)
+		{
+			if(root!=null)
+			{
+				TraverseInOrder(root.Left);
+				root.DisplayNode();
+				TraverseInOrder(root.Right);
+			}
+		}
+
+		public void TraversePreOrder(BSTNode root)
+		{
+			if(root!=null)
+			{
+				root.DisplayNode();
+				TraversePreOrder(root.Left);
+				TraversePreOrder(root.Right);
+			}
+		}
+
+		public void TraversePostOrder(BSTNode root)
+		{
+			if(root!=null)
+			{
+				TraversePreOrder(root.Left);
+				TraversePreOrder(root.Right);
+				root.DisplayNode();
+			}
+		}
+	}
+}
---
+++ Algorithims/DataStructure/BSTNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithims.DataStructure
+{
+	public class BSTNode
+	{
+		private int _data;
+		private BSTNode _left;
+		private BSTNode bSTNode;
+
+		public BSTNode Right
+		{
+			get { return bSTNode; }
+			set { bSTNode = value; }
+		}
+
+
+		public BSTNode Left
+		{
+			get { return _left; }
+			set { _left = value; }
+		}
+
+		public int Data
+		{
+			get { return _data; }
+			set { _data = value; }
+		}
+
+
+		public void DisplayNode()
+		{
+			Console.Write(_data);
+		}
+	}
+}
