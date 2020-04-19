1c7201bddcdeb455180bbbdd8ec09c88a0d262d8
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-04-19 18:54:18 -0400

    Graph

---
+++ Algorithims/DataStructure/Graph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithims.DataStructure
+{
+	public class Graph<T>
+	{
+
+		private List<Node> _nodes;
+
+		public class Node
+		{
+			private T _data;
+
+			public T Data
+			{
+				get { return _data; }
+				set { _data = value; }
+			}
+
+			private List<Node> _children;
+
+			public Node(T data)
+			{
+				_children = new List<Node>();
+			}
+
+		}
+
+		public Graph()
+		{
+			_nodes = new List<Node>();
+		}
+	}
+}
